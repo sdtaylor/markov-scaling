33f11ac0b5b5f2a758bdf1858ac28130ed057691
Shawn <shawntaylor@weecology.org>
2016-04-26 14:46:01 -0400

    build the actual markov model from quadrat data

--- wooton_scale_analysis.R
+++ wooton_scale_analysis.R
@@ -9,6 +9,7 @@ spatial_scales=c(2,4,8,10,15,30)
 
 
 #######################################
+#Load data
 spp_codes=read.csv('./data/SpeciesCodes.txt', sep='\t') %>%
   select(CODE, AGGREGATED.GROUP)
 
@@ -22,6 +23,17 @@ transect_data=read.csv('./data/Tatoosh_Intertidal_Transitions_Transects.txt', se
   mutate(month=substring(date, 1,3), year=as.integer(substring(date, 5,6))) %>% #Make dates usuable
   mutate(year=ifelse(year<90, year+2000,year+1900))
 
+
+quad_data=read.csv('./data/Tatoosh_Intertidal_Transitions_Quadrats.txt', sep='\t') %>%
+  select(-X) %>%
+  gather(date, species, -Quadrat, -Point.Number, -Point.Letter) %>%
+  left_join(spp_codes, by=c('species'='CODE')) %>% #Convert species to summarized ones defined in spp file
+  select(-species) %>%
+  rename(species=AGGREGATED.GROUP) %>%
+  filter(species!='X', !is.na(species)) %>%
+  mutate(month=substring(date, 1,3), year=as.integer(substring(date, 5,6))) %>% #make dates usable
+  mutate(year=ifelse(year<90, year+2000,year+1900))  %>%
+  select(-date)
 
 #######################################
 #Setup the spatial scales to model at
@@ -51,3 +63,44 @@ for(this_spatial_scale in spatial_scales){
   
   set_id=set_id+1
 }
+
+rm(this_spatial_scale, num_replicates, replicate_identifier, this_transect, temp_df,i)
+#####################################
+#Build the matrix model out of the quad data.
+
+#Only doing 1 annual transition instead of the summer/winter transition
+quad_data = quad_data %>%
+  filter(month %in% c('May','Jul','Jun')) %>%
+  select(-month)
+
+quad_data_next_year = quad_data %>%
+  mutate(year=year-1) %>%
+  rename(species_next_year=species)
+  
+transitions_temp = quad_data %>%
+  left_join(quad_data_next_year, by=c('Quadrat','Point.Number','Point.Letter','year')) %>%
+  filter(!is.na(species_next_year)) #Some years are missing. Plus the final year can't be included. 
+
+#Get probabilites for each species to each other species
+transitions = data.frame()
+for(this_species in unique(transitions_temp$species)){
+  x=transitions_temp %>%
+    filter(species==this_species) %>%
+    count(species_next_year) %>%
+    mutate(n=n/sum(n), species=this_species) 
+  
+  transitions = transitions %>%
+    bind_rows(x)
+}
+
+#Make it an actual matrix. cols are speces at t, rows are prob. of becoming that speces at t+1
+transitions= transitions %>%
+  spread(species, n) 
+
+rownames(transitions) = transitions$species_next_year
+transitions = transitions %>%
+  select(-species_next_year) %>%
+  as.matrix()
+
+rm(quad_data_next_year, transitions_temp, this_species, x)
+########################################################
