0167b65fcb7df40835b406511140149a9dfe5db2
Shawn <shawntaylor@weecology.org>
2016-04-29 12:43:36 -0400

    added in temporal scaling

--- wooton_scale_run_models.R
+++ wooton_scale_run_models.R
@@ -9,8 +9,8 @@ library(magrittr)
 #spatial_scales=c(1,5,10)
 spatial_scales=c(1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55,60,65,70)
 
-#The number of years to average together and predict.
-temporal_scales=c(1,2,3)
+#The temporal scales to average average results over.
+temporal_scales=c(1,2,5,10,19)
 
 results_file='./results/results_wooton.csv'
 #######################################
@@ -32,6 +32,9 @@ transect_data=read.csv('./data/Tatoosh_Intertidal_Transitions_Transects.txt', se
   mutate(point_id=paste(Transect, Point.Number, sep='-')) %>% #Interpret multiple transects as one giant transect
   select(-Transect, -Point.Number)
 
+#Drop some poorly sampled transect points
+transect_data = transect_data %>%
+  filter(!point_id %in% c('SFN-18','ES-12','LF-25'))
 
 quad_data=read.csv('./data/Tatoosh_Intertidal_Transitions_Quadrats.txt', sep='\t') %>%
   select(-X) %>%
@@ -75,53 +78,62 @@ for(this_spatial_scale in spatial_scales){
     replicate_identifier = c(replicate_identifier, -1)
   }
   
-
-  temp_df=data.frame(point_id=all_point_ids, replicate=replicate_identifier,
-                     'spatial_scale'=this_spatial_scale, set=set_id)
-  model_sets = model_sets %>%
+  #Repeat for every temporal scale
+  for(this_temporal_scale in temporal_scales){
+    
+    temp_df=data.frame(point_id=all_point_ids, replicate=replicate_identifier,
+                       'spatial_scale'=this_spatial_scale, 'temporal_scale'=this_temporal_scale,
+                       set=set_id)
+    model_sets = model_sets %>%
       bind_rows(temp_df)
+    set_id=set_id+1
+    
+  }
   
-  
-  set_id=set_id+1
 }
 
 model_sets = model_sets %>%
   filter(replicate >0)
 
-rm(this_spatial_scale, num_replicates, replicate_identifier, num_points, all_point_ids, temp_df,i)
+rm(this_spatial_scale, this_temporal_scale, num_replicates, replicate_identifier, num_points, all_point_ids, temp_df,i)
 #####################################
-#Build the matrix model out of the quad data.
-
-quad_data_next_year = quad_data %>%
-  mutate(year=year-1) %>%
-  rename(species_next_year=species)
+#Build the markov matrix model 
+# training_data is a df of c('point_id','species','year')
+# temporal_scale is integer
+build_transition_matrix=function(training_data){
+  data_next_year = training_data %>%
+    mutate(year=year-1) %>%
+    rename(species_next_timestep = species)
   
-transitions_temp = quad_data %>%
-  left_join(quad_data_next_year, by=c('point_id','year')) %>%
-  filter(!is.na(species_next_year)) #Some years are missing. Plus the final year can't be included. 
-
-#Get probabilites for each species to each other species
-transitions = data.frame()
-for(this_species in unique(transitions_temp$species)){
-  x=transitions_temp %>%
-    filter(species==this_species) %>%
-    count(species_next_year) %>%
-    mutate(n=n/sum(n), species=this_species) 
+  transitions_temp = training_data %>%
+    left_join(data_next_year, by=c('point_id','year')) %>%
+    filter(!is.na(species_next_timestep)) #Some years are missing. Plus the final year can't be included. 
   
+  #Get probabilites for each species to each other species
+  transitions = data.frame()
+  for(this_species in unique(transitions_temp$species)){
+    x=transitions_temp %>%
+      filter(species==this_species) %>%
+      count(species_next_timestep) %>%
+      mutate(n=n/sum(n), species=this_species) 
+    
+    transitions = transitions %>%
+      bind_rows(x)
+  }
+  
+  #Make it an actual matrix. cols are speces at t, rows are prob. of becoming that speces at t+1
+  transitions= transitions %>%
+    spread(species, n) 
+  
+  rownames(transitions) = transitions$species_next_year
   transitions = transitions %>%
-    bind_rows(x)
+    select(-species_next_timestep) %>%
+    as.matrix()
+  
+  return(transitions)
 }
 
-#Make it an actual matrix. cols are speces at t, rows are prob. of becoming that speces at t+1
-transitions= transitions %>%
-  spread(species, n) 
 
-rownames(transitions) = transitions$species_next_year
-transitions = transitions %>%
-  select(-species_next_year) %>%
-  as.matrix()
-
-rm(quad_data_next_year, transitions_temp, this_species, x)
 ########################################################
 #Run the markov model given a set of inital conditions and timesteps.
 #Return a timeseries of community composition
@@ -212,14 +224,44 @@ create_composition_timeseries=function(df){
 }
 
 ###########################################################
+#Temporal scales. Take the output from timesteps of 1 year, and average predictions and observations
+#over sets of n years
+apply_temporal_scale=function(composition, temporal_scale){
+  if(temporal_scale==1){
+    return(composition)
+  }
+  
+  new_num_cols=floor(ncol(composition) / temporal_scale)
+  
+  new_matrix=matrix(nrow=nrow(composition), ncol=new_num_cols)
+  
+  #If na data is present (from missing years), don't do averages for smaller time scales
+  if(temporal_scale < 4){excludeNA=FALSE} else {excludeNA=TRUE}
+  
+  for(i in seq(new_num_cols)){
+    original_timesteps_to_include = ((temporal_scale*i)-temporal_scale+1) : (temporal_scale*i)
+    new_matrix[,i] = rowMeans(composition[,original_timesteps_to_include], na.rm=excludeNA)
+    
+  }
+  colnames(new_matrix) = 1:new_num_cols
+  return(new_matrix)
+  
+}
+
+###########################################################
 #Iterate through all model sets. making predictions from the model thru time, comparing results
 #and compiling everything in a df
 run_analysis=function(){
+  
+  transitions=build_transition_matrix(quad_data)
+  
   final_results=data.frame()
   for(this_set in sort(unique(model_sets$set))){
     this_set_list = model_sets %>%
       filter(set==this_set) 
     
+    this_spatial_scale = unique(this_set_list$spatial_scale)
+    this_temporal_scale = unique(this_set_list$temporal_scale)
     for(this_replicate in sort(unique(this_set_list$replicate))){
 
       this_replicate_point_ids= this_set_list %>%
@@ -231,10 +273,21 @@ run_analysis=function(){
       actual_composition=transect_data %>%
         filter(point_id %in% this_replicate_point_ids) %>%
         create_composition_timeseries()
-        
+      
+      #Some of spatial subsets aren't sampled across the entire time series.   
+      if(ncol(actual_composition) < this_temporal_scale){
+        break
+      }
+      
       predicted_composition = run_model(transitions, actual_composition[,1], ncol(actual_composition))
+      
+      #Temporal averaging here
+      actual_composition=apply_temporal_scale(actual_composition, this_temporal_scale)
+      predicted_composition=apply_temporal_scale(predicted_composition, this_temporal_scale)
+      
       results_this_replicate=compare_composition(actual_composition, predicted_composition)
         
+      
       results_this_replicate$set=this_set
       results_this_replicate$replicate=this_replicate
         
@@ -246,7 +299,7 @@ run_analysis=function(){
   }
 
   final_results = final_results %>%
-    left_join( select(model_sets, set, spatial_scale) %>% distinct(), by='set') %>%
+    left_join( select(model_sets, set, spatial_scale, temporal_scale) %>% distinct(), by='set') %>%
     filter(!is.na(mse)) #some na values from missing year
   
   return(final_results)
